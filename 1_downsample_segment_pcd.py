75154ec9636b8b772bd49e8539aaef4d6170cfb4
Sarah Edelaar <sarah.edelaar@gmail.com>
2024-10-17 17:16:28 +0200

    rename method comparing

--- 1_downsample_segment_pcd.py
+++ 1_downsample_segment_pcd.py
@@ -4,6 +4,8 @@ from sklearn.cluster import KMeans
 import pyvista as pv
 import os
 from datetime import datetime
+import matplotlib.pyplot as plt
+from kneed import KneeLocator
 
 # Step 1: Read the PLY file
 point_cloud = o3d.io.read_point_cloud(r"C:\Users\sarah\PycharmProjects\CoreKnapenGit\scans\Scan_46_20241015_161510_filtered.ply")
@@ -23,12 +25,42 @@ downsampled_pc.orient_normals_towards_camera_location(camera_location=[0, 0, 0])
 o3d.visualization.draw_geometries([downsampled_pc], point_show_normal=False)
 o3d.visualization.draw_geometries([downsampled_pc], point_show_normal=True)
 
-# Access normals programmatically
+# Step 5: Access the normals
 normals = np.asarray(downsampled_pc.normals)
 
-# Step 7: KMeans clustering of normals
-# Specify the number of clusters you expect
-n_clusters = 3
+
+def elbow_method(normals, max_k=10, save_path="elbow_plot.png"):
+    inertia_values = []
+    k_range = range(1, max_k + 1)
+
+    for k in k_range:
+        kmeans = KMeans(n_clusters=k, random_state=0).fit(normals)
+        inertia_values.append(kmeans.inertia_)
+
+    # Find the elbow point using KneeLocator
+    knee_locator = KneeLocator(k_range, inertia_values, curve='convex', direction='decreasing')
+    optimal_k = knee_locator.elbow  # This is the elbow point
+
+    # Plot the elbow graph
+    plt.plot(k_range, inertia_values, 'bx-')
+    plt.xlabel('Number of clusters (k)')
+    plt.ylabel('Inertia')
+    plt.title(f'Elbow Method for Optimal k (Elbow at k={optimal_k})')
+    plt.axvline(x=optimal_k, color='red', linestyle='--')
+    plt.show()
+
+    # Save the plot as an image file
+    plt.savefig(save_path, format='png', dpi=300)
+    print(f"Elbow plot saved as {save_path}")
+
+    return optimal_k
+
+
+# Automatically choose the optimal number of clusters
+n_clusters = elbow_method(normals, max_k=10)
+print(f'amount of clusters: {n_clusters}')
+
+# Step 8: Apply KMeans clustering using the chosen number of clusters
 clustering = KMeans(n_clusters=n_clusters, random_state=0).fit(normals)
 
 # Step 8: Get the cluster labels
@@ -54,12 +86,12 @@ for i, cluster in enumerate(clusters):
     if mean_normal[2] <= upward_threshold:  # Check if the Z-component is below or equal to the threshold
         non_upward_clusters.append(cluster)
 
-# # Uncomment the lines below to save the clusters to a folder if needed
-# timestamp = datetime.now().strftime('%m%d_%H%M')
-# output_folder = rf"C:\Users\sarah\PycharmProjects\CoreKnapenGit\segmented_point_clouds\segmentation_{timestamp}"
-# os.makedirs(output_folder, exist_ok=True)
-# for i, cluster in enumerate(non_upward_clusters):
-#     o3d.io.write_point_cloud(rf"{output_folder}/non_upward_cluster_{i}.ply", cluster)
+# Uncomment the lines below to save the clusters to a folder if needed
+timestamp = datetime.now().strftime('%m%d_%H%M')
+output_folder = rf"C:\Users\sarah\PycharmProjects\CoreKnapenGit\segmented_point_clouds\segmentation_{timestamp}"
+os.makedirs(output_folder, exist_ok=True)
+for i, cluster in enumerate(non_upward_clusters):
+    o3d.io.write_point_cloud(rf"{output_folder}/non_upward_cluster_{i}.ply", cluster)
 
 # Step 11: Visualize the non-upward clusters using PyVista
 plotter = pv.Plotter()
