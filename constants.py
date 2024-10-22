c866fe43162a1de5e19cc72564556eaccf7c60b6
Sarah Edelaar <sarah.edelaar@gmail.com>
2024-10-22 09:37:49 +0200

    structuur versie 3_22-10

--- constants.py
+++ constants.py
@@ -1,12 +1,21 @@
 # Point Cloud processing
 FILE_DIR_PCD = r'C:\Users\sarah\PycharmProjects\CoreKnapenGit\scans'
 FILE_NAME_PCD = 'Scan_46_20241015_161510_filtered.ply'
+
+# Voxel downsampling
 VOXEL_SIZE = 0.01
+
+# Radius outlier removal
 NB_POINTS = 15
 RADIUS = 0.02
 
+# DBSCAN clustering
+EPS = 0.02
+MIN_SAMPLES = 15
+MIN_POINTS = 50
+
 # Mesh processing
-FILE_DIR_MESH = ''
-FILE_NAME_MESH = ''
+FILE_DIR_MESH = r'C:\Users\sarah\PycharmProjects\CoreKnapenGit\comparing_model'
+FILE_NAME_MESH_LIST = ['composite_meshes2-1_15-10.ply', 'composite_meshes2-2_15-10.ply']
 
 # Comparing Mesh and Point Cloud
